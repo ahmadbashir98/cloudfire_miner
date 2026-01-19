52bd5560014bbb4ac0253799af38a7839054e4a6
engrzani <zeenateducator@gmail.com>
2026-01-19 23:30:29 +0500

    Wrap Express app as Vercel serverless function

--- api/index.js
+++ api/index.js
@@ -1,3 +1,11 @@
-const handler = require('../dist/index.cjs');
-
-module.exports = handler.default || handler;
+// Vercel serverless function handler
+module.exports = async (req, res) => {
+  // Import the built Express app
+  const app = require('../dist/index.cjs');
+  
+  // Get the default export or the app itself
+  const handler = app.default || app;
+  
+  // Execute the Express app
+  return handler(req, res);
+};
